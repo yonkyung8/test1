eefc2cf5936a616b85a7d6b0276d81503bdc5186
yonkyung8 <choiyon13400@gmail.com>
2025-11-13 04:53:39 +0800

    Adjust cactus dimensions in CACTI_CONFIG

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const GROUND_HEIGHT = 24;
 const GROUND_AND_CACTUS_SPEED = 0.5;
 
 const CACTI_CONFIG = [
-  { width: 48 / 1.5, height: 100 / 1.5, image: "images/cactus_1.png" },
+  { width: 48 / 1.3, height: 100 / 1.3, image: "images/cactus_1.png" },
   { width: 98 / 1.5, height: 100 / 1.5, image: "images/cactus_2.png" },
-  { width: 68 / 1.5, height: 70 / 1.5, image: "images/cactus_3.png" },
+  { width: 80 / 1.2, height: 80 / 1.2, image: "images/cactus_3.png" },
 ];
 
 //Game Objects
